71f818cfcba4384d25da15a429b99f5d5bb04712
Nick Moore <nicholasvmoore@gmail.com>
2018-08-28 03:53:45 +0000

    adding the u-boot precompiled mainline and the updated run script

--- run.sh
+++ run.sh
@@ -3,34 +3,36 @@
 ## Make a truncated sparse disk image
 #truncate -s 8G backup.img
 
-export image="backup.img"
+export backup_image_location="/mnt/lun0/temp"
+export backup_image_file="backup.img"
+export backup_mount="/media/backup"
 export subvol_root="/media/backup/subvol_root"
 export loopback_device="/dev/loop0"
 
 # Create a backup image file
-truncate -s 8G
+truncate -s 5G $backup_image_location/$backup_image_file
 
 # Partition the image file
-parted $image mklabel msdos
-parted $image mkpart primary -a optimal 2048s 128M
-parted backup.img mkpart primary -a optimal btrfs 128M 4.5G
-losetup -D
-losetup -P $loopback_device backup.img
+parted $backup_image_location/$backup_image_file mklabel msdos
+parted $backup_image_location/$backup_image_file mkpart primary -a optimal 2048s 128M
+parted $backup_image_location/$backup_image_file mkpart primary -a optimal btrfs 128M 5G
+losetup -d $loopback_device
+losetup -P $loopback_device $backup_image_location/$backup_image_file  
 
 # Format the partitions
 mkfs.vfat -n boot ${loopback_device}p1
 mkfs.btrfs -L rootfs ${loopback_device}p2
 
 ## Mount the image
-mount -o rw,loop,compress=zstd,noatime,ssd ${loopback_device}p2 /media/backup
+mkdir $backup_mount
+mount -o rw,loop,compress=zstd,noatime,ssd ${loopback_device}p2 $backup_mount
 btrfs sub cr $subvol_root
 mkdir -p $subvol_root/media/boot
 mkdir -p $subvol_root/boot
 
-# Create root subvolume
-btrfs sub cr subvol_root ${loopback_device}p2
+# Create boot mount
 mount -o rw,loop ${loopback_device}p1 $subvol_root/boot
-umount -R /media/backup
+umount -R $backup_mount
 
 # Rsync
 rsync -axAX /media/boot/ $subvol_root/media/boot/
@@ -38,11 +40,11 @@ rsync -axAX /boot/ $subvol_root/boot
 rsync -axAX / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $subvol_root/
 
 # u-boot mainline
-git/u-boot/sd_fuse/sd_fusing.sh $loopback_device
+u-boot/sd_fuse/sd_fusing.sh $loopback_device
 
 # clean-up
-umount -R /media/backup
-losetup -D
+#umount -R 
+#losetup -d $loopback_device
 
 ## Compress the output
-zstd backup.img
+#zstd $image
