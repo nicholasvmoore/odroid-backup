a791a5ad4a5e4d76c1f02c6b245035bf7f067d5c
Nicholas Moore <nicholasvmoore@gmail.com>
2018-09-08 09:52:30 -0600

    cleaning up the script and adding functions

--- run.sh
+++ run.sh
@@ -3,44 +3,60 @@
 ## Make a truncated sparse disk image
 #truncate -s 8G backup.img
 
-export backup_image_location="/mnt/lun0/temp"
-export backup_image_file="backup.img"
-export backup_mount="/media/backup"
-export subvol_root="/media/backup/subvol_root"
-export loopback_device="/dev/loop0"
+backup_image_location="/mnt/lun0/temp"
+backup_image_file="backup.img"
+backup_mount="/media/backup"
+subvol_root="/media/backup/subvol_root"
+loopback_device="/dev/loop0"
+
+function partition_image {
+	# Partition the image file
+	parted $backup_image_location/$backup_image_file mklabel msdos
+	parted $backup_image_location/$backup_image_file mkpart primary -a optimal 2048s 128M
+	parted $backup_image_location/$backup_image_file mkpart primary -a optimal btrfs 128M 5G
+}
+
+function format_image {
+	# Format the partitions
+	mkfs.vfat -n boot ${loopback_device}p1
+	mkfs.btrfs -L rootfs ${loopback_device}p2
+}
+
+function backup_machine {
+	# Rsync
+	rsync -axAX /media/boot/ $subvol_root/media/boot/
+	rsync -axAX /boot/ $subvol_root/boot
+	rsync -axAX / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $subvol_root/
+}
+
+function u-boot_image {
+	( cd sd_fuse && ./sd_fusing.sh $loopback_device )
+}
+
+function clean-up {
+	umount -R 
+	losetup -d $loopback_device
+}
 
 # Create a backup image file
 truncate -s 5G $backup_image_location/$backup_image_file
+partition_image
 
-# Partition the image file
-parted $backup_image_location/$backup_image_file mklabel msdos
-parted $backup_image_location/$backup_image_file mkpart primary -a optimal 2048s 128M
-parted $backup_image_location/$backup_image_file mkpart primary -a optimal btrfs 128M 5G
+# Setup loopback
 losetup -d $loopback_device
-losetup -P $loopback_device $backup_image_location/$backup_image_file  
+losetup -P $loopback_device $backup_image_location/$backup_image_file
+format_image
 
-# Format the partitions
-mkfs.vfat -n boot ${loopback_device}p1
-mkfs.btrfs -L rootfs ${loopback_device}p2
-
-## Mount the image
+## Mount the images
 mkdir $backup_mount
 mount -o rw,loop,compress=zstd,noatime,ssd ${loopback_device}p2 $backup_mount
 btrfs sub cr $subvol_root
 mkdir -p $subvol_root/media/boot
 mkdir -p $subvol_root/boot
 
-# Rsync
-rsync -axAX /media/boot/ $subvol_root/media/boot/
-rsync -axAX /boot/ $subvol_root/boot
-rsync -axAX / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $subvol_root/
-
-# )u-boot mainline
-( cd sd_fuse && ./sd_fusing.sh $loopback_device )
-
-# clean-up
-#umount -R 
-#losetup -d $loopback_device
+backup_machine
+u-boot_image
+clean-up
 
 ## Compress the output
-#zstd $image
+zstd $image
